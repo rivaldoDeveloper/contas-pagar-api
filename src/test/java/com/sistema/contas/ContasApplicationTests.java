2e0b63d28ad4edcf8539febc68ba1c03f1861604
rivaldoDeveloper <rivaldo.website@gmail.com>
2026-05-26 02:55:33 -0300

    Subindo classe de testes

--- src/test/java/com/sistema/contas/ContasApplicationTests.java
+++ src/test/java/com/sistema/contas/ContasApplicationTests.java
@@ -2,8 +2,10 @@ package com.sistema.contas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Garante que o application-test.yml (ou application.yml em test/resources) seja carregado
 class ContasApplicationTests {
 
 	@Test
